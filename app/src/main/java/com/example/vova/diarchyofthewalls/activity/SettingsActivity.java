59e3f3d7ba85cb39d2317f8efc05758dd3b5068a
mishakras <irinabudtova59@gmail.com>
2018-01-23 20:02:25 +0300

    misha

--- app/src/main/java/com/example/vova/diarchyofthewalls/activity/SettingsActivity.java
+++ app/src/main/java/com/example/vova/diarchyofthewalls/activity/SettingsActivity.java
@@ -2,14 +2,45 @@ package com.example.vova.diarchyofthewalls.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 
 import com.example.vova.diarchyofthewalls.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity  implements SeekBar.OnSeekBarChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+
+        mTextView = (TextView)findViewById(R.id.Sound);
+        mTextView.setText("Sound 0");
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        mTextView.setText(String.valueOf(seekBar.getProgress()));
     }
 }
