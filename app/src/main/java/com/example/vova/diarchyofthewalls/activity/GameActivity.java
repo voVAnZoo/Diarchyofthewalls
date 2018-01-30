2128e50c5f5fbc5c679282e815e406edf24235ef
mishakras <irinabudtova59@gmail.com>
2018-01-30 17:44:01 +0300

    misha

--- app/src/main/java/com/example/vova/diarchyofthewalls/activity/GameActivity.java
+++ app/src/main/java/com/example/vova/diarchyofthewalls/activity/GameActivity.java
@@ -3,8 +3,13 @@ package com.example.vova.diarchyofthewalls.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.view.View.OnTouchListener;
+import android.widget.ImageButton;
+
+import com.example.vova.diarchyofthewalls.core.Entity;
 
 import com.example.vova.diarchyofthewalls.R;
 
@@ -16,13 +21,16 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         final Button Pause = (Button) findViewById(R.id.PauseBt);
-        final Button lef = (Button) findViewById(R.id.LeftBt);
-        final Button rig = (Button) findViewById(R.id.RightBt);
+        final ImageButton lef = (ImageButton) findViewById(R.id.LeftBt);
+        final ImageButton rig = (ImageButton) findViewById(R.id.RightBt);
         final Button ite = (Button) findViewById(R.id.itemBt);
         final Button Eqi = (Button) findViewById(R.id.EqBt);
+        final Button chal = (Button) findViewById(R.id.ChangeLvlBt);
+        final Button chas = (Button) findViewById(R.id.ChangeBt);
         final Intent pau = new Intent(this, PauseActivity.class);
         final Intent item = new Intent(this, ItemActivity.class);
-
+        chal.setEnabled(false);
+        chas.setEnabled(false);
 
         Pause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +42,29 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(item);
+            }
+        });
+        lef.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (MotionEvent.getAction()) {
+                    case MotionEvent.ACTION_BUTTON_PRESS:
+
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return false;
+            }
+        });
+        rig.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (MotionEvent.getAction()) {
+                    case MotionEvent.ACTION_BUTTON_PRESS:
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+                return false;
             }
         });
     }
