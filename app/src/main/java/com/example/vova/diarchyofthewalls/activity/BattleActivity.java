702448c5023c2b5c872d7a451c4caab3d4843a82
mishakras <irinabudtova59@gmail.com>
2018-02-03 22:21:39 +0300

    mm

--- app/src/main/java/com/example/vova/diarchyofthewalls/activity/BattleActivity.java
+++ app/src/main/java/com/example/vova/diarchyofthewalls/activity/BattleActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -15,7 +16,12 @@ import com.example.vova.diarchyofthewalls.core.Player;
 
 public class BattleActivity extends AppCompatActivity {
 
-    int turn;
+    public  TextView AttackAction;
+    public  TextView DefenceAction;
+    public  TextView EnemyAction;
+    public  TextView Turn;
+
+    int turn=0;
 
     int kn1 = 0;
     int kn4 = 0;
@@ -25,13 +31,13 @@ public class BattleActivity extends AppCompatActivity {
     boolean acid;
     boolean fire;
 
-    boolean damup;
-    double defup;
+    boolean damup=false;
+    double defup=0;
 
-    boolean flag1;
-    boolean flag2;
-    boolean flag3;
-    boolean flag4;
+    boolean flag1=true;
+    boolean flag2=true;
+    boolean flag3=true;
+    boolean flag4=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +53,17 @@ public class BattleActivity extends AppCompatActivity {
         final ImageButton DamageUp = (ImageButton) findViewById(R.id.DamageUpBt);
         final ImageButton Evasion = (ImageButton) findViewById(R.id.EvasionBt);
 
+        AttackAction = (TextView) findViewById(R.id.AttackAction);
+        DefenceAction= (TextView) findViewById(R.id.DefenceAction);
+        EnemyAction= (TextView) findViewById(R.id.EnemyAction);
+        Turn = (TextView) findViewById(R.id.Turn);
+
         final Intent EndGame = new Intent(this, MainActivity.class);
 
-
-
         while (p.hp > 0 && e.hp > 0) {
+            ++turn;
+            defup=0;
+            Turn.setText("@string/Turn" + turn);
             if (acid)
                 p.hp=p.hp-2;
             if (fire)
@@ -74,19 +86,9 @@ public class BattleActivity extends AppCompatActivity {
                         Damage.setImageResource(R.drawable.right);
                     else
                         Damage.setImageResource(R.drawable.left);
-                    flag1 = !flag1; }
-            });
-            Defence.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    kn2 = turn;
-                    defup = 10;
-                    Defence.setEnabled(false);
-                    if (flag2)
-                        Defence.setImageResource(R.drawable.right);
-                    else
-                        Defence.setImageResource(R.drawable.left);
-                    flag2 = !flag2; }
+                    flag1 = !flag1;
+                    AttackAction.setText("@string/AtackAction1");
+                }
             });
             Damage.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -99,7 +101,23 @@ public class BattleActivity extends AppCompatActivity {
                         Damage.setImageResource(R.drawable.right);
                     else
                         Damage.setImageResource(R.drawable.left);
-                    flag3 = !flag3;}
+                    flag3 = !flag3;
+                    AttackAction.setText("@string/AtackAction2");
+                }
+            });
+            Defence.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    kn2 = turn;
+                    defup = 10;
+                    Defence.setEnabled(false);
+                    if (flag2)
+                        Defence.setImageResource(R.drawable.right);
+                    else
+                        Defence.setImageResource(R.drawable.left);
+                    flag2 = !flag2;
+                    DefenceAction.setText("@string/DefenceAction1");
+                }
             });
             Evasion.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -111,15 +129,20 @@ public class BattleActivity extends AppCompatActivity {
                         Evasion.setImageResource(R.drawable.right);
                     else
                         Evasion.setImageResource(R.drawable.left);
-                    flag4 = !flag4;}
+                    flag4 = !flag4;
+                    DefenceAction.setText("@string/DefenceAction1");
+                }
             });
-            if (turn % 3 == 1)
+            if (turn % 3 == 1) {
                 acid = true;
-            if (turn % 3 == 2)
+
+            }
+            if (turn % 3 == 2) {
                 fire = true;
-            if (turn % 3 == 0 && e.damage > defup / 2)
-                p.hp = p.hp - e.damage + defup / 2;
-            ++turn;
+            }
+            if (turn % 3 == 0 && e.damage > defup / 2){
+                    p.hp = p.hp - e.damage + defup / 2;
+            }
         }
         if (p.hp <= 0)
             startActivity(EndGame);
